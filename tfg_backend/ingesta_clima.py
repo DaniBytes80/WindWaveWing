f393b1361aa38ee7b0c648bfecaa172836b0a114
Daniel Molina Pérez <danibytes.milaulas@gmail.com>
2026-05-23 20:56:15 +0200

    Change SUPABASE_SERVICE_ROLE_KEY to SUPABASE_KEY in ingesta_clima.py

--- tfg_backend/ingesta_clima.py
+++ tfg_backend/ingesta_clima.py
@@ -5,7 +5,7 @@ import os
 
 # --- Conexión a Supabase ---
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # --- Funciones auxiliares ---
