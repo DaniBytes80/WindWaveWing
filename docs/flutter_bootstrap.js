d612bf560c7d4167644b8d4c007ba8e80921da37
DaniBytes80 <danibytes.milaulas@gmail.com>
2026-06-14 22:27:09 +0200

    Deploy web version

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1651512779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "368706659" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
